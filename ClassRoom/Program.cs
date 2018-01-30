734d798a792c6063eb5749631e2389dff66c18ab
Christoffer G. Thomsen <chris@cgt.name>
2018-01-30 10:07:04 +0100

    Solve task 3

--- ClassRoom/Program.cs
+++ ClassRoom/Program.cs
@@ -10,6 +10,16 @@ namespace ClassRoom
     {
         static void Main(string[] args)
         {
+            var classRoom = new ClassRoom
+            {
+                Name = "3A",
+                SemesterStart = new DateTime(2017, 8, 28),
+                Students = new List<Student>
+                {
+                    new Student("Christoffer G. Thomsen", 6, 21),
+                    new Student("Foo", 1, 1),
+                },
+            };
         }
     }
 }
