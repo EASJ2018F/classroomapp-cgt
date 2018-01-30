8438165d851b2da6f18794e767bdabea05c2e72c
Christoffer G. Thomsen <chris@cgt.name>
2018-01-30 11:58:47 +0100

    Solve task 5
    I placed the Season method in the Program class. It could have been in
    the Student class, but ideally it would be a property of the DateTime
    class. It didn't seem right to place it in the Student class and I did
    not want to subclass DateTime, so I simply made it a static method in
    the Program class.
    
    The Season method takes a Student as its argument, but a better, more
    general solution would have been to make it take a month number instead.
    It returns a hard-coded string for each season, but it would probably be
    a better solution to create an enum with values for each season and
    return that instead.

--- ClassRoom/Program.cs
+++ ClassRoom/Program.cs
@@ -29,5 +29,28 @@ namespace ClassRoom
                 Console.WriteLine($"\t{s.Name} (birth day/month): {s.BirthDay}/{s.BirthMonth}");
             }
         }
+
+        static string Season(Student s)
+        {
+            switch (s.BirthMonth)
+            {
+                case 1:
+                case 2:
+                case 12:
+                    return "Winter";
+                case 3:
+                case 4:
+                case 5:
+                    return "Spring";
+                case 6:
+                case 7:
+                case 8:
+                    return "Summer";
+                case 9:
+                case 10:
+                case 11:
+                    return "Fall";
+            }
+        }
     }
 }
